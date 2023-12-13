3074513dde4e0052ccafb7eb8a704e1facfe5b4d
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2023-12-13 10:43:47 +0100

    Update GameRenderer.h

--- FrogGameEngine/GameRenderer.h
+++ FrogGameEngine/GameRenderer.h
@@ -4,6 +4,27 @@
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/gtx/rotate_vector.hpp"
 
+struct Plane
+{
+	// unit vector
+	vec3 normal = { 0.0f, 1.0f, 0.0f };
+
+	// distance from origin to the nearest point in the plane
+	// (?) distance only to z
+	double distance = 0.0f;
+};
+
+struct Frustum
+{
+	Plane topFace;
+	Plane bottomFace;
+
+	Plane rightFace;
+	Plane leftFace;
+
+	Plane farFace;
+	Plane nearFace;
+};
 
 struct Camera
 {
@@ -15,6 +36,31 @@ struct Camera
 	vec3 eye;
 	vec3 center;
 	vec3 up;
+
+	/*Frustum createFrustum()	{
+		Frustum     frustum;
+
+		const vec3 front = glm::normalize(eye - center);
+		const vec3 right = glm::cross(glm::normalize(up), front);
+
+		const float halfVSide = zFar * tanf(fov * .5f);
+		const float halfHSide = halfVSide * aspect;
+		const vec3 frontMultFar = zFar * front;
+
+
+		frustum.nearFace = { front, eye + zNear * front };
+		frustum.farFace = { eye + frontMultFar, -front };
+		frustum.rightFace = { eye,
+								glm::cross(frontMultFar - right * halfHSide, up) };
+		frustum.leftFace = { eye,
+								glm::cross(up, frontMultFar + right * halfHSide) };
+		frustum.topFace = { eye,
+								glm::cross(right, frontMultFar - up * halfVSide) };
+		frustum.bottomFace = { eye,
+								glm::cross(frontMultFar + up * halfVSide, right) };
+
+		return frustum;
+	}*/
 
 	mat4 computeLookAt() const {
 		return glm::lookAt(eye, center, up);
