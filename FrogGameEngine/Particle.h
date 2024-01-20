1e98ff558ed5b48f8abf73bf25633a597d438e23
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2024-01-20 16:44:34 +0100

    Update Particle.h

--- FrogGameEngine/Particle.h
+++ FrogGameEngine/Particle.h
@@ -1,3 +1,28 @@
 #pragma once
 // only has information of itself
 // the emmiter can change part of its values
+
+// instead of having initColor and endColor, having deltaColor and multiplying it with dt and adding it to the color
+
+#include "Types.h"
+
+#include "glm/gtc/quaternion.hpp"
+#include "glm/gtx/quaternion.hpp"
+
+
+class Particle {
+public:
+	float lifetime;
+
+	// Color color
+	// Color deltaColor
+
+	vec3 position;
+	glm::quat rotation;
+	vec3 scale;
+
+
+
+private: 
+	
+};
