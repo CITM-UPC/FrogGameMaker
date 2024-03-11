efedb74ba4d1672d348a246781757a5f67b57aec
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2024-03-11 14:09:20 +0100

    billboarding working

--- FrogGameEngine/BillboardingEM.cpp
+++ FrogGameEngine/BillboardingEM.cpp
@@ -13,26 +13,31 @@ BillboardRender::BillboardRender(Emmiter* owner)
 
 void BillboardRender::Update(Particle* particle, vec3 cameraPosition)
 {
+    glPushMatrix();
+    glTranslatef(particle->position.x, particle->position.y, particle->position.z);
+
 	vec3 particlePosition = particle->position + owner->owner->GetTransform()->getPosition();
 
-	Billboard::BeginCylindricBillboard(particlePosition, cameraPosition);
+	Billboard::BeginSphericalBillboard(particlePosition, cameraPosition);
 
     // render
     glBegin(GL_TRIANGLES);
     glColor3ub(particle->color.r, particle->color.g, particle->color.b);
 
-    glVertex3d(particle->position.x - (1 * particle->scale.x), particle->position.y + (1 * particle->scale.y), particle->position.z);
-    glVertex3d(particle->position.x - (1 * particle->scale.x), particle->position.y - (1 * particle->scale.y), particle->position.z);
-    glVertex3d(particle->position.x + (1 * particle->scale.x), particle->position.y + (1 * particle->scale.y), particle->position.z);
-
-    glVertex3d(particle->position.x - (1 * particle->scale.x), particle->position.y - (1 * particle->scale.y), particle->position.z);
-    glVertex3d(particle->position.x + (1 * particle->scale.x), particle->position.y - (1 * particle->scale.y), particle->position.z);
-    glVertex3d(particle->position.x + (1 * particle->scale.x), particle->position.y + (1 * particle->scale.y), particle->position.z);
+    glVertex3d(- (1 * particle->scale.x), + (1 * particle->scale.y), 0);
+    glVertex3d(- (1 * particle->scale.x), - (1 * particle->scale.y), 0);
+    glVertex3d(+ (1 * particle->scale.x), + (1 * particle->scale.y), 0);
+                                                                     
+    glVertex3d(- (1 * particle->scale.x), - (1 * particle->scale.y), 0);
+    glVertex3d(+ (1 * particle->scale.x), - (1 * particle->scale.y), 0);
+    glVertex3d(+ (1 * particle->scale.x), + (1 * particle->scale.y), 0);
 
     glEnd();
 
 	// particle->Render();
 
 	Billboard::EndBillboard();
+
+    //glPopMatrix();
 
 }
